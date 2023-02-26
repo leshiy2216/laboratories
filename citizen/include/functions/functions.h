982909427d2938f6d28793baf117b51c9d1de591
leshiy2216 <smbeybulatov@gmail.com>
2023-02-26 23:32:20 +0400

    LittleStep

--- citizen/include/functions/functions.h
+++ citizen/include/functions/functions.h
@@ -1,3 +1,26 @@
 #pragma once
+#include <string>
+#include <iostream>
+using namespace std;
 
 int sum_stub(int lhs, int rhs);
+
+enum HumanState
+{
+	schooler,
+	student,
+	retire
+};
+
+class human 
+{
+public:
+
+	string flname;
+	string schoolname;
+	int ticket_number;
+
+private:
+
+	HumanState State;
+};
--- citizen/src/functions.cc
+++ citizen/src/functions.cc
@@ -1,5 +1,13 @@
 #include <functions/functions.h>
 
-int sum_stub(int lhs, int rhs) {
+int SchoolPayday(int lhs, int rhs) {
+    return lhs + rhs;
+}
+
+int StudentlPayday(int lhs, int rhs) {
+    return lhs + rhs;
+}
+
+int RetirePayday(int lhs, int rhs) {
     return lhs + rhs;
 }
