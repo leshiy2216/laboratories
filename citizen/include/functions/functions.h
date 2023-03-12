ba5d86304cb70aed07de8d17287b83293121a1c3
leshiy2216 <smbeybulatov@gmail.com>
2023-03-12 13:32:12 +0400

    change

--- citizen/include/functions/functions.h
+++ citizen/include/functions/functions.h
@@ -1,92 +1,74 @@
 #pragma once
 #include <string>
 #include <iostream>
+#define MINSIZE 16242
 using namespace std;
-
-int sum_stub(int lhs, int rhs);
 
 namespace citizen
 {
 
-	const int MinSize = 16242;
-
 	enum HumanType
 	{
-		schooler,
-		student,
-		retire
-	};
-
-	enum AverageScore
-	{
-		Bad,
-		Average,
-		Good,
-		Better,
-		Perfectly
+		SCHOOLER,
+		STUDENT,
+		RETIRE
 	};
 
 	class Human
 	{
+	private:
+		HumanType type;
+		string name;
+		string PlaceName;
+		int ticket;
+		bool is_large_family;
+		int book;
+		double score;
+		int ID;
+		int years;
 
 	public:
-
-		// general
-		string FLname;
-
-		// schooler
-		string school;
-		int TicketNumber;
-		bool flag;
-		Human(string FLname, string school, int TicketNumber, bool flag)
-		{
-			FLname = FLname;
-			school = school;
-			TicketNumber = TicketNumber;
-			flag = flag;
-		}
-		double SchoolPayday(int flag);
-
-		// student
-		string university;
-		int StudentID;
-		double AverageScore;
-		Human(string FLname, string university, int StudentID, double AverageScore)
-		{
-			FLname = FLname;
-			university = university;
-			StudentID = StudentID;
-			AverageScore = AverageScore;
-		}
-		double StudentPayday(double AverageScore);
-
-		// retire
-		int IDcard;
-		int experience;
-		Human(string FLname, int IDcard, int experience)
-		{
-			FLname = FLname;
-			IDcard = IDcard;
-			experience = experience;
-		}
-		double RetirePayday(int experience);
-
-	private:
-		HumanType Type;
-
-		Human(HumanType Type, string FLname, string school, int TicketNumber, bool flag, string university, int StudentID, double AverageScore, int IDcard, int experience)
-		{
-			this->Type = Type;
-			this->FLname = FLname;
-			this->school = school;
-			this->TicketNumber = TicketNumber;
-			this->flag = flag;
-			this->university = university;
-			this->StudentID = StudentID;
-			this->AverageScore = AverageScore;
-			this->IDcard = IDcard;
-			this->experience = experience;
-		}
+		Human(HumanType type, string name, string PlaceName, int ticket, bool is_large_family);
+		Human(HumanType type, string name, string PlaceName, int book, double score);
+		Human(HumanType type, string name, int ID, int years);
+		void set_type(HumanType type);
+		void set_name(string name);
+		void set_PlaceName(string PlaceName);
+		void set_ticket(int ticket);
+		void set_IsLargeFamily(bool is_large_family);
+		void set_book(int book);
+		void set_score(double score);
+		void set_ID(int ID);
+		void set_years(int years);
+		HumanType get_type();
+		string get_name();
+		string get_PlaceName();
+		int get_ticket();
+		bool get_IsLargeFamily();
+		int get_book();
+		double get_score();
+		int get_ID();
+		int get_years();
+		double payday_schooler();
+		double payday_student();
+		double payday_retire();
 	};
 
+	class HumanList
+	{
+	private:
+		static const int CAPACITY = 10;
+		Human persons[CAPACITY];
+		int size;
+
+	public:
+		HumanList();
+		HumanList(Human person[], int size);
+		HumanList(Human person[], int size, float);
+		HumanList(Human person[], int size, int);
+		Human get_person_with_index(int i);
+		void set_size(int size);
+		int get_size();
+		void create_person(int index, Human person);
+	};
 }
--- citizen/src/functions.cc
+++ citizen/src/functions.cc
@@ -1,63 +1,198 @@
 #include <functions/functions.h>
-#define MinSize 16242
+#include <string>
 using namespace citizen;
 using namespace std;
 
-
-double Human::SchoolPayday(int flag)
+Human(HumanType type, string name, string PlaceName, int ticket, bool is_large_family)
 {
-    if (flag == 1)
-    {
-        return 0.5 * MinSize;
-    }
-    return 0;
+	this->type = type;
+	this->name = name;
+	this->PlaceName = PlaceName;
+	this->ticket = ticket;
+	this->is_large_family = is_large_family;
 }
 
-double Human::StudentPayday(double AverageScore)
+Human(HumanType type, string name, string PlaceName, int book, double score)
 {
-    if (AverageScore < 3.5)
-    {
-        return 0;
-    }
-    if (AverageScore == 3.5)
-    {
-        return 0.5 * MinSize;
-    }
-    if (AverageScore == 4.0)
-    {
-        return 0.6 * MinSize;
-    }
-    if (AverageScore == 4.5)
-    {
-        return 0.7 * MinSize;
-    }
-    if (AverageScore == 5.0)
-    {
-        return 0.8 * MinSize;
-    }
-
+	this->type = type;
+	this->name = name;
+	this->PlaceName = PlaceName;
+	this->book = book;
+	this->score = score;
 }
 
-double Human::RetirePayday(int experience)
+Human(HumanType type, string name, int ID, int years)
 {
-    if (experience < 5)
-    {
-        return 0;
-    }
-    if (experience <= 10 && experience >= 5)
-    {
-        return MinSize;
-    }
-    if (experience <= 20 && experience > 10)
-    {
-        return 0.2 * MinSize;
-    }
-    if (experience <= 25 && experience > 20)
-    {
-        return 0.3 * MinSize;
-    }
-    if (experience <= 30 && experience > 25)
-    {
-        return 0.4 * MinSize;
-    }
+	this->type = type;
+	this->name = name;
+	this->ID = ID;
+	this->years = years;
+}
+
+void set_type(HumanType type)
+{
+	this->type = type;
+}
+void set_name(string name)
+{
+	this->name = name;
+}
+void set_PlaceName(string PlaceName)
+{
+	this->PlaceName = PlaceName;
+}
+void set_ticket(int ticket)
+{
+	this->ticket = ticket;
+}
+void set_IsLargeFamily(bool is_large_family)
+{
+	this->is_large_family = is_large_family;
+}
+void set_book(int book)
+{
+	this->book = book;
+}
+void set_score(double score)
+{
+	this->score = score;
+}
+void set_ID(int ID)
+{
+	this->ID = ID;
+}
+void set_years(int years)
+{
+	this->years = years;
+}
+
+HumanType get_type()
+{
+	return type;
+}
+string get_name()
+{
+	return name;
+}
+string get_PlaceName()
+{
+	return PlaceName;
+}
+int get_ticket()
+{
+	return ticket;
+}
+bool get_IsLargeFamily()
+{
+	return IsLargeFamily;
+}
+int get_book()
+{
+	return book;
+}
+double get_score()
+{
+	return score;
+}
+int get_ID()
+{
+	return ID;
+}
+int get_years()
+{
+	return years;
+}
+
+double payday_schooler()
+{
+	if (IsLargeFamily) return double payment = 0.5 * MINSIZE;
+	else return 0;
+}
+
+double payday_student()
+{
+	if (score < 3.5) return 0;
+	if (score >= 3.5 && score < 4) return double payment = 0.5 * MINSIZE;
+	if (score >= 4 && score < 4.5) return double payment = 0.6 * MINSIZE;
+	if (score >= 4.5 && score < 5) return double payment = 0.7 * MINSIZE;
+	if (score == 5) return double payment = 0.8 * MINSIZE;
+}
+
+double payday_retire()
+{
+	if (years < 5) return 0;
+	if (years > 30) return double payment = 5 * MINSIZE;
+	if (years >= 5 && years < 10) return MINSIZE;
+	if (years >= 10 && years < 20) return double payment = 2 * MINSIZE;
+	if (years >= 20 && years <= 25) return double payment = 3 * MINSIZE;
+	if (years > 25 && years <= 30) return double payment = 4 * MINSIZE;
+}
+
+HumanList()
+{
+	this->size = 7;
+	for (int i = 0; i < size; ++i)
+	{
+		persons[i] = Human();
+	}
+}
+
+HumanList(Human person[], int size)
+{
+	this->size = size;
+	for (int i = 0; i < size; ++i)
+	{
+		this->persons[i].set_type(person[i].get_type());
+		this->persons[i].set_name(person[i].get_name());
+		this->persons[i].set_PlaceName(person[i].get_PlaceName());
+		this->persons[i].set_ticket(person[i].get_ticket());
+		this->persons[i].set_IsLargeFamily(person[i].get_IsLargeFamily());
+	}
+}
+HumanList(Human person[], int size)
+{
+	this->size = size;
+	for (int i = 0; i < size; ++i)
+	{
+		this->persons[i].set_type(person[i].get_type());
+		this->persons[i].set_name(person[i].get_name());
+		this->persons[i].set_PlaceName(person[i].get_PlaceName());
+		this->persons[i].set_book(person[i].get_book());
+		this->persons[i].set_score(person[i].get_score());
+	}
+}
+HumanList(Human person[], int size)
+{
+	this->size = size;
+	for (int i = 0; i < size; ++i)
+	{
+		this->persons[i].set_type(person[i].get_type());
+		this->persons[i].set_name(person[i].get_name());
+		this->persons[i].set_ID(person[i].get_ID());
+		this->persons[i].set_years(person[i].get_years());
+	}
+}
+
+Human get_person_with_index(int i)
+{
+	return persons[i];
+}
+
+int get_size()
+{
+	return size;
+}
+void set_size(int size)
+{
+	this->size = size;
+}
+
+void create_person(int index, Human person)
+{
+	for (int i = size - 1; i >= index; --i)
+	{
+		persons[i] = persons[i - 1];
+	}
+	persons[index] = person;
+	++size;
 }
